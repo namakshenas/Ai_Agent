4e38ab6c9165083f223111875d63ad14020d6e86
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-03 05:09:16 +0200

    *add* front / chat conversations service

---
+++ src/services/ChatConversationsService.ts
@@ -0,0 +1,37 @@
+import { IChatHistoryQAPair } from "../interfaces/IChatHistoryQAPair";
+
+
+export class ChatConversationsService{
+
+    static chatConversations : TChatConversation[] = [];
+    // lastContext should be added too !!!! >> all context in reality so question can be modified
+
+    static pushConversation(chatHistory : IChatHistoryQAPair[]){
+        this.chatConversations.push(chatHistory);
+        return;
+    }
+
+    static getConversation(id : number) : IChatHistoryQAPair[]{
+        return this.chatConversations[id]
+    }
+
+    static deleteConversation(id : number){
+        this.chatConversations.splice(id,1);
+        return;
+    }
+
+    static updateConversation(id : number, chatHistory : IChatHistoryQAPair[]) {
+        this.chatConversations[id] = chatHistory;
+        return;
+    }
+
+    static getConversations(){
+        return this.chatConversations;
+    }
+
+    static getNumberOfConversations() : number{
+        return this.chatConversations.length
+    }
+}
+
+type TChatConversation = IChatHistoryQAPair[]
