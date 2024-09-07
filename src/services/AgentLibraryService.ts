0f2a142648dcd846cdeaa7198a08979e534dcdaf
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-07 04:22:21 +0200

    *edit* front / new autocomplete assistant added to the agentlibrary

--- src/services/AgentLibraryService.ts
+++ src/services/AgentLibraryService.ts
@@ -2,10 +2,15 @@ import { AIAgent } from "../models/AIAgent"
 
 export class AgentLibraryService {
     static #agentsLibrary = new Map()
-
-    /*constructor() { 
-        this.addAgent(new AIAgent('HelpfulAssistantAgent'))
-    }*/
+    /*.set('helpfulAssistant', new AIAgent("helpfulAssistant"))
+    .set('autocompleteAssistant', new AIAgent("autocompleteAssistant").setSystemPrompt(
+        `You are an auto-complete bot.
+        As such, your only role is to generate the text needed to complete the last sentence of a given piece of text.
+        Don't add any annotations, comments, marking or delimiters.
+        Just reply with the string needed to complete the piece of text.
+        Here comes the piece of text :\n\n
+        `
+    ))*/
 
     static addAgent(agent: AIAgent) {
         this.#agentsLibrary.set(agent.name, agent)
@@ -13,6 +18,10 @@ export class AgentLibraryService {
 
     static removeAgent(agentName: string): boolean {
         return this.#agentsLibrary.delete(agentName)
+    }
+
+    static removeAllAgents(){
+        this.#agentsLibrary = new Map()
     }
     
     static getAgentsNameList(): Array<string> {
