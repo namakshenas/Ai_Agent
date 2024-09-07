d13c78034b055c37e56e6dde561cfd079c88dfd4
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-07 04:21:54 +0200

    *edit* front / autocomplete method added to chatservice

--- src/services/ChatService.ts
+++ src/services/ChatService.ts
@@ -11,7 +11,7 @@ export class ChatService{
      */
     static async askTheActiveModel(question : string, context:number[] = []) : Promise<{context : number[], response : string}>
     {
-        console.log('question : '+ question)
+        // console.log('question : '+ question)
         const model = new AIModel({modelName : "llama3.1:8b"}).setTemperature(0.1).setContextSize(8000).setContext(context).setSystemPrompt("You are an helpful assistant.")
         const answer = (await model.ask(question))
         return {context : answer.context as number[], response : answer.response}
@@ -29,5 +29,25 @@ export class ChatService{
         console.log('question : '+ question)
         const model = new AIModel({modelName : "llama3.1:8b"}).setTemperature(0.1).enableStreaming().setContextSize(8000).setContext(context).setSystemPrompt("You are an helpful assistant.")
         return await model.askForAStreamedResponse(question)
+    }
+
+    static async askTheActiveModelForAutoComplete(promptToComplete : string, context:number[] = []) : Promise<{context : number[], response : string}>
+    {
+        const model = new AIModel({modelName : "llama3.1:8b"}).setTemperature(0.1).setContextSize(8000).setContext(context)
+        .setSystemPrompt(`You are specialized in questions auto-completion.
+        As such, your role is to produce an output completing the last sentence of a given piece of text. 
+        Your output should always be a string of characters transforming the incomplete last sentence into a question.
+        Your output should NEVER BE AN ANSWER to this incomplete sentence.
+        If the last sentence is already a valid question then you should reply with an empty string.
+        Here is some examples showing you have to behave:\n\n
+        * last sentence example : "Should I buy a" => output : " new car?"
+        * last sentence example : "Who is the president " => output : "of the United States?"
+        * last sentence example : "What is the capit" => output : "al of France?"
+        * last sentence example : "What is the name of Batman?" => output : ""
+        !!! ONLY RESPOND WITH THE OUTPUT NOT THE FULL SENTENCE.
+        Here follows the given piece of text to produce an output for :\n\n
+        `)
+        const answer = (await model.ask(promptToComplete))
+        return {context : answer.context as number[], response : answer.response}
     }
 }
