6bd22ee75b9a532c126ecd5b9ca576dc9fa6cb3a
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-11 07:05:49 +0200

    *edit* front / docker file : unquote vite config to unlock it

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  /*server: {
+    host: true, // Allows access from outside the container
+    port: 5173, // Change this if needed
+  },*/
 })
