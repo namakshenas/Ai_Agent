ec1e473ba18386f930ae5eb96a364522e6f7ff3c
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-19 11:30:31 +0200

    *add* front / base modal integration

---
+++ src/components/Modal.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ReactNode, useEffect, useRef } from 'react'
+import './Modal.css'
+
+function Modal({children, modalVisibility, setModalVisibility, /*modalContent,*/ containerCSSClass} : IProps){
+
+    const dialogRef = useRef<HTMLDialogElement>(null)
+    const modalVisibilityRef = useRef<boolean>(modalVisibility)
+    
+    useEffect(() => {
+        if(modalVisibilityRef && !dialogRef.current?.open) return dialogRef.current?.showModal()
+        if(!modalVisibilityRef && dialogRef.current?.open) return dialogRef.current?.close()
+    })
+    
+    // needs to pass setModalVisibility to modalContent
+    return (
+        modalVisibility 
+        ? <dialog data-testid="modal" ref={dialogRef} onClick={(e) => { if (e.target === dialogRef.current) setModalVisibility(false) }}>
+            {children}
+        </dialog> 
+        : <></>
+    )
+}
+
+export default Modal
+
+interface IProps{
+    children: ReactNode
+    modalVisibility : boolean
+    // modalContent : JSX.Element
+    containerCSSClass? : string
+    setModalVisibility : (bool : boolean) => void
+}
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -13,9 +13,9 @@ import CustomTextarea, { ImperativeHandle } from "../components/CustomTextarea";
 import { ActionType, useConversationReducer } from "../hooks/useConversationReducer";
 import { CustomCheckbox } from "../components/CustomCheckbox/CustomCheckbox";
 import internetIcon from '../assets/search-engine.png';
-import stopIcon from '../assets/stopIcon2.png';
 import { WebSearchService } from "../services/WebSearchService";
 import Select, { IOption } from "../components/CustomSelect/Select";
+import Modal from "../components/Modal";
 
 
 function Chat() {
@@ -36,11 +36,19 @@ function Chat() {
 
     const [isStreaming, setIsStreaming] = useState<boolean>(false)
 
-    const [isWebSearchActivated, setWebSearchActivated] = useState(false)
+    const [isWebSearchActivated, _setWebSearchActivated] = useState(false)
+    const isWebSearchActivatedRef = useRef<boolean>(false)
+
+    const [modalVisibility, setModalVisibility] = useState<boolean>(false)
 
     function setTextareaValue(value: string) {
         textareaValueRef.current = value
         _setTextareaValue(value)
+    }
+
+    function setWebSearchActivated(value: boolean) {
+        isWebSearchActivatedRef.current = value
+        _setWebSearchActivated(value)
     }
 
     const modelsList = useFetchModelsList()
@@ -66,19 +74,20 @@ function Chat() {
     // asking the model for a streamed response
     async function handleSendMessage_Streaming(message: string): Promise<void> {
         try {
-            console.log("call")
             if (message == "") return
             const currentContext = conversationStateRef.current.history[conversationStateRef.current.history.length - 1]?.context || []
             setIsStreaming(true)
             dispatch({ type: ActionType.NEW_BLANK_HISTORY_ELEMENT, payload: message })
             let newContext
-            if (isWebSearchActivated) {
-                console.log("websearch")
-                const scrapedPages = await WebSearchService.getRelatedWebpagesDatas(message)
-                newContext = await ChatService.askTheActiveAgentForAStreamedResponse(message, pushStreamedAnswerToHistoryCallback, currentContext, scrapedPages)
-                dispatch({ type: ActionType.ADD_SOURCES_TO_LAST_ANSWER, payload: scrapedPages.map((page) => page.source) })
+            if (isWebSearchActivatedRef.current) {
+                console.log("**WebScraping**")
+                const scrapedPages = await WebSearchService.scrapeRelatedDatas(message)
+                console.log("**LLM Loading**")
+                newContext = await ChatService.askTheActiveAgentForAStreamedResponse(message, pushStreamedAnswerToHistory_Callback, currentContext, scrapedPages)
+                dispatch({ type: ActionType.ADD_SOURCES_TO_LAST_ANSWER, payload: scrapedPages })
             } else {
-                newContext = await ChatService.askTheActiveAgentForAStreamedResponse(message, pushStreamedAnswerToHistoryCallback, currentContext)
+                console.log("**LLM Loading**")
+                newContext = await ChatService.askTheActiveAgentForAStreamedResponse(message, pushStreamedAnswerToHistory_Callback, currentContext)
             }
             clearTextAreaIfQuestionReplied(conversationStateRef.current.history[conversationStateRef.current.history.length - 1].question, textareaValueRef.current)
             setIsStreaming(false)
@@ -96,7 +105,7 @@ function Chat() {
     }
 
     // callback passed to the chatservice to display the streamed answer / !!! should be renamed answerSaverCallback
-    function pushStreamedAnswerToHistoryCallback(contentAsMarkdown: string, contentAsHTML: string): void {
+    function pushStreamedAnswerToHistory_Callback(contentAsMarkdown: string, contentAsHTML: string): void {
         dispatch({ type: ActionType.UPDATE_LAST_HISTORY_ANSWER, payload: { html: contentAsHTML, markdown: contentAsMarkdown } })
     }
 
@@ -112,7 +121,15 @@ function Chat() {
 
     function handleSearchWebClick(e: React.MouseEvent<HTMLDivElement>) {
         e.preventDefault()
-        setWebSearchActivated(!isWebSearchActivated)
+        setWebSearchActivated(!isWebSearchActivatedRef.current)
+    }
+
+    function handleScrollToTopClick(){
+        document.getElementById("selectAgentLabel")?.scrollIntoView({ behavior: "smooth" })
+    }
+
+    function handleAgentSettingsClick() {
+        setModalVisibility(currentVisibility => !currentVisibility)
     }
 
     return (
@@ -124,10 +141,8 @@ function Chat() {
                 <select className="agentDropdown" onChange={(e) => ChatService.setActiveAgent(e.target.value)}>
                     {agentsList.map((agent, id) => <option key={'agent' + id}>{agent}</option>)}
                 </select>
-                <button style={{ padding: '0 0.85rem' }}>
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M12.035 0.809018C12.405 0.543018 12.925 0.419018 13.436 0.606018C14.5223 1.00418 15.5313 1.58784 16.418 2.33102C16.835 2.68102 16.988 3.19202 16.942 3.64402C16.867 4.39702 16.999 5.12402 17.362 5.75002C17.682 6.30702 18.164 6.74702 18.752 7.05702L18.977 7.16702C19.391 7.35402 19.759 7.74302 19.852 8.28002C20.0507 9.41808 20.0507 10.582 19.852 11.72C19.769 12.204 19.462 12.567 19.099 12.771L18.977 12.834C18.287 13.144 17.723 13.624 17.361 14.25C16.999 14.877 16.867 15.603 16.942 16.356C16.987 16.808 16.835 17.32 16.418 17.669C15.5313 18.4122 14.5223 18.9959 13.436 19.394C13.2038 19.4771 12.9548 19.502 12.7108 19.4666C12.4667 19.4312 12.235 19.3366 12.036 19.191C11.42 18.75 10.723 18.5 9.99999 18.5C9.27699 18.5 8.57999 18.749 7.96499 19.191C7.76584 19.3368 7.53399 19.4315 7.28976 19.4669C7.04552 19.5023 6.79633 19.4772 6.56399 19.394C5.47769 18.9959 4.46872 18.4122 3.58199 17.669C3.39414 17.5095 3.24818 17.3065 3.15685 17.0776C3.06551 16.8488 3.03157 16.601 3.05799 16.356C3.13299 15.603 2.99999 14.876 2.63799 14.25C2.3089 13.6906 1.82658 13.2371 1.24799 12.943L1.02299 12.833C0.797289 12.7331 0.59951 12.5795 0.446968 12.3854C0.294426 12.1914 0.191772 11.9629 0.147993 11.72C-0.0507144 10.582 -0.0507144 9.41808 0.147993 8.28002C0.230993 7.79602 0.537993 7.43302 0.900993 7.22902L1.02299 7.16702C1.71299 6.85602 2.27699 6.37702 2.63899 5.75002C2.99999 5.12402 3.13299 4.39702 3.05799 3.64402C3.03157 3.39902 3.06551 3.15128 3.15685 2.92241C3.24818 2.69354 3.39414 2.4905 3.58199 2.33102C4.46872 1.58784 5.47769 1.00418 6.56399 0.606018C6.79619 0.522965 7.04519 0.498042 7.28924 0.533429C7.53328 0.568816 7.76495 0.663436 7.96399 0.809018C8.57899 1.25102 9.27599 1.50002 9.99999 1.50002C10.724 1.50002 11.42 1.25102 12.035 0.809018ZM12.992 2.57802C12.126 3.14802 11.105 3.50002 9.99999 3.50002C8.89499 3.50002 7.87399 3.14702 7.00799 2.57802C6.31188 2.85914 5.65881 3.23684 5.06799 3.70002C5.12799 4.73302 4.92299 5.79302 4.37099 6.75002C3.81799 7.70602 3.00299 8.41302 2.07799 8.87802C1.97275 9.62165 1.97275 10.3764 2.07799 11.12C3.00299 11.585 3.81799 12.292 4.37099 13.25C4.92299 14.205 5.12799 15.265 5.06799 16.298C5.65875 16.7615 6.31183 17.1396 7.00799 17.421C7.87399 16.851 8.89499 16.499 9.99999 16.499C11.105 16.499 12.126 16.852 12.992 17.421C13.6881 17.1399 14.3412 16.7622 14.932 16.299C14.872 15.265 15.077 14.205 15.629 13.249C16.181 12.292 16.997 11.585 17.922 11.119C18.027 10.376 18.027 9.62199 17.922 8.87902C16.997 8.41302 16.182 7.70602 15.629 6.74902C15.077 5.79302 14.872 4.73302 14.932 3.69902C14.3412 3.23584 13.6881 2.85914 12.992 2.57802ZM9.99999 6.00002C11.0609 6.00002 12.0783 6.42145 12.8284 7.17159C13.5786 7.92174 14 8.93915 14 10C14 11.0609 13.5786 12.0783 12.8284 12.8284C12.0783 13.5786 11.0609 14 9.99999 14C8.93913 14 7.92171 13.5786 7.17157 12.8284C6.42142 12.0783 5.99999 11.0609 5.99999 10C5.99999 8.93915 6.42142 7.92174 7.17157 7.17159C7.92171 6.42145 8.93913 6.00002 9.99999 6.00002ZM9.99999 8.00002C9.46956 8.00002 8.96085 8.21073 8.58578 8.5858C8.21071 8.96088 7.99999 9.46959 7.99999 10C7.99999 10.5305 8.21071 11.0392 8.58578 11.4142C8.96085 11.7893 9.46956 12 9.99999 12C10.5304 12 11.0391 11.7893 11.4142 11.4142C11.7893 11.0392 12 10.5305 12 10C12 9.46959 11.7893 8.96088 11.4142 8.5858C11.0391 8.21073 10.5304 8.00002 9.99999 8.00002Z" fill="#000000aa" />
-                    </svg>
+                <button style={{ padding: '0 0.85rem' }} onClick={handleAgentSettingsClick}>
+                    <svg style={{width:'18px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 416c0 17.7 14.3 32 32 32l54.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 448c17.7 0 32-14.3 32-32s-14.3-32-32-32l-246.7 0c-12.3-28.3-40.5-48-73.3-48s-61 19.7-73.3 48L32 384c-17.7 0-32 14.3-32 32zm128 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM320 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32-80c-32.8 0-61 19.7-73.3 48L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l246.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48l54.7 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-54.7 0c-12.3-28.3-40.5-48-73.3-48zM192 128a32 32 0 1 1 0-64 32 32 0 1 1 0 64zm73.3-64C253 35.7 224.8 16 192 16s-61 19.7-73.3 48L32 64C14.3 64 0 78.3 0 96s14.3 32 32 32l86.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 128c17.7 0 32-14.3 32-32s-14.3-32-32-32L265.3 64z"/></svg>
                 </button>
                 <button style={{ paddingLeft: '0.75rem', paddingRight: '0.75rem' }}>+ New</button>
             </div>
@@ -144,12 +159,16 @@ function Chat() {
                         Search the web
                         <img style={{opacity:0.6}} src={internetIcon} width="18px" height="18px" />
                     </div>
-                    <button onClick={() => document.getElementById("selectAgentLabel")?.scrollIntoView({ behavior: "smooth" })}>a</button>
+                    <button onClick={handleScrollToTopClick}>Filled context : {conversationStateRef.current.history[conversationStateRef.current.history.length - 1]?.context.length}</button>
                     <button onClick={() => handleSendMessage_Streaming(textareaValue)}>Send</button>
-                    {isStreaming && <button className="cancelSendButton" onClick={handleAbortStreamingClick}><img style={{opacity:0.6, width:'20px'}} src={stopIcon}/></button>}
+                    {isStreaming && <button className="cancelSendButton" onClick={handleAbortStreamingClick}>
+                        <svg style={{opacity:1, width:'20px', flexShrink:0}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg>
+                    </button>}
                 </div>
             </div>
-            {<div>Filled context : {conversationStateRef.current.history[conversationStateRef.current.history.length - 1]?.context.length}</div>}
+            <Modal modalVisibility={modalVisibility} setModalVisibility={setModalVisibility}>
+                <div>aaaaaaaaaaaaa</div>
+            </Modal>
         </>
     )
 }
@@ -161,21 +180,25 @@ export default Chat
 // download model config + agents from other users using a dedicated website & api
 // downvoting a reply should get it out of context
 // collapse previous history
-// when clicking on modify question, jump to textarea
 // refresh three questions with a button and close three questions
 // stop autoscroll down when streaming if the user scroll up
 // since now autoscroll, put the cancel button next to the send button
 // deal with ollama request not manually aborted leading to cancel request button not disappearing
 // switching conversation shouldnt lead to scrollbottom : check useeffect triggered by history state update
 // conversation button blinking when switching conversation
-// deal with json failing on some streaming
+// deal with json failing on some streaming => incomplete context array
 // issue : switching between conversations when the conversation hasn't been saved in chatconvservice
 // edit question sometimes takes two clicks
 // browse through previous questions / scroll to the question?
 // should be able to open a panel under the textarea to tweak the current model settings
-// add <a> to sources
 // fix issue with ollama last streamed element with sometimes incomplete context
 // should summarize the scraped data used to reply the answer
+// when switching conversation, it generates new suggestions, if clicking x times on diffrent conversation, i can have x suggestions
+// generations in queue, so need to abort
+// determine if the request can be without the use of web searching
+// in one tab, i should be able to schedule a recurrent data research on a topic to stay informed
+// should add pictures too with the reply
+// clipboard for code blocks
 
 /*
 Valid Parameters and Values
