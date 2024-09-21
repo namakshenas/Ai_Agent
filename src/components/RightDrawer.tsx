fa5903ca64fc45a628ab88ea8be20f0410ee10f0
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-21 12:36:11 +0200

    *edit* front / base right drawer added

---
+++ src/components/RightDrawer.tsx
@@ -0,0 +1,14 @@
+import './RightDrawer.css'
+
+export default function RightDrawer(){
+    return(
+        <aside className="rightDrawer">
+            <ul>
+                <li>Conversation 1</li>
+                <li>Conversation 1</li>
+                <li>Conversation 1</li>
+                <li>Conversation 1</li>
+            </ul>
+        </aside>
+    )
+}
