6eac3f18a97128bbec764336e9999467587c0eed
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-22 11:51:35 +0200

    *add* front / custom hook : usemodalmanager

---
+++ src/hooks/useModalManager.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* c8 ignore start */
+import { useState, useEffect } from "react"
+
+function useModalManager({initialVisibility, initialModalContentId} : IModalObject) {
+
+    const [modalVisibility, setModalVisibility] = useState<boolean>(initialVisibility)
+    const [modalContentId, setModalContentId] = useState<string>(initialModalContentId)
+
+    useEffect(() => {
+  
+        function keyboardListener(e : KeyboardEvent){
+            if(e.code == "Escape" && modalVisibility) {e.preventDefault(); setModalVisibility(false)}
+        }
+
+        window.addEventListener('keydown', keyboardListener)
+
+        // clean up to avoid having two listeners active => since useEffect is triggered twice in strict mode
+        return () => {
+            window.removeEventListener('keydown', keyboardListener)
+        }
+
+    }, [])
+
+    useEffect(() => {
+
+        if(modalVisibility) {
+            scrollLock(true)
+        } else { 
+            scrollLock(false)
+        }
+
+    }, [modalVisibility])
+
+    return { modalVisibility, setModalVisibility, modalContentId, setModalContentId }
+}
+
+export default useModalManager
+
+interface IModalObject{
+    initialVisibility : boolean
+    initialModalContentId : string
+}
+
+function scrollLock(bool : boolean)
+{
+    if(bool === true)
+    {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        window.onscroll = () => {
+            window.scrollTo(scrollLeft, scrollTop)
+        }
+    }else{
+        window.onscroll = () => {}
+    }
+}
