42809d086138ec7ac30792f7686512a2eee6d678
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-23 12:37:56 +0200

    *edit* front / right drawer form fully functional

--- src/types/IListModelResponse.ts
+++ src/types/IListModelResponse.ts
@@ -1,10 +1,14 @@
 import { IModelDetails } from "./IModelDetails";
 
 export interface IListModelResponse {
+        models : IModelInfos[]
+}
+
+interface IModelInfos{
         name: string;
         model: string;
         modifiedAt: Date;
         size: number;
         digest: string;
-        details?: IModelDetails;
+        details?: IModelDetails;  
 }
