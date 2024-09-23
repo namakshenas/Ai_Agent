e11531668b433e3333d00a02bb9e05a199578ee0
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-09-23 12:35:04 +0200

    *add* front / get running models out of the ollama api + related interface

--- src/services/OllamaService.ts
+++ src/services/OllamaService.ts
@@ -1,7 +1,8 @@
 import { IListModelResponse } from "../types/IListModelResponse";
+import { IRunningModelsResponse } from "../types/IRunningModelResponse";
 
 export class OllamaService{
-    static async getModelList() : Promise<{ models : IListModelResponse[]} | undefined>{
+    static async getModelList() : Promise<IListModelResponse | undefined>{
         const url = "http://127.0.0.1:11434/api/tags"
         try {
             const response = await fetch(url);
@@ -14,4 +15,44 @@ export class OllamaService{
             console.error(error);
         }
     }
+
+    static async getRunningModelInfos() : Promise<IRunningModelsResponse | undefined>{
+        const url = "http://127.0.0.1:11434/api/ps"
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+              throw new Error(`Response status: ${response.status}`);
+            }
+            const json = await response.json();
+            return json
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
+
+/*
+
+{
+  "models": [
+    {
+      "name": "mistral:latest",
+      "model": "mistral:latest",
+      "size": 5137025024,
+      "digest": "2ae6f6dd7a3dd734790bbbf58b8909a606e0e7e97e94b7604e0aa7ae4490e6d8",
+      "details": {
+        "parent_model": "",
+        "format": "gguf",
+        "family": "llama",
+        "families": [
+          "llama"
+        ],
+        "parameter_size": "7.2B",
+        "quantization_level": "Q4_0"
+      },
+      "expires_at": "2024-06-04T14:38:31.83753-07:00",
+      "size_vram": 5137025024
+    }
+  ]
+}
+*/
---
+++ src/types/IRunningModelResponse.ts
@@ -0,0 +1,20 @@
+export interface IRunningModelsResponse {
+    models : IRunningModelInfo[]
+}
+
+interface IRunningModelInfo {
+    "name": string
+    "model": string
+    "size": number
+    "digest": string
+    "details": {
+      "parent_model": string
+      "format": string
+      "family": string
+      "families": string[]
+      "parameter_size": string
+      "quantization_level": string
+    },
+    "expires_at": string
+    "size_vram": number
+}
