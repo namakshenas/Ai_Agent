9d3c7ada6b93669c564ed1ed7b235d2c7ff34db4
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-10-03 17:58:30 +0200

    *edit* front / memoized left panel

--- src/components/LeftPanel/LeftPanel.tsx
+++ src/components/LeftPanel/LeftPanel.tsx
@@ -5,24 +5,34 @@ import DocumentsSlot from './DocumentsSlot'
 import { ConversationsSlot } from './ConversationsSlot'
 import { PromptsSlot } from './PromptsSlot'
 import { useEffect } from 'react'
+import React from 'react'
+import { IConversation } from '../../interfaces/IConversation'
+import IPrompt from '../../interfaces/IPrompt'
 
-export default function LeftPanel({activeConversation, setActiveConversation, setModalStatus, selectedPromptRef} : IProps){
+// export default function LeftPanel({activeConversation, setActiveConversation, setModalStatus, selectedPromptRef} : IProps){
+const LeftPanel = React.memo(({activeConversation, conversationStateRef, setActiveConversation, memoizedSetModalStatus, promptsList, selectedPromptRef} : IProps) => {
 
-    useEffect(() => {console.log("left panel render")}, [])
+    useEffect(() => {console.log("left panel render")})
 
     return(
         <aside className="leftDrawer">
             <figure style={{cursor:'pointer'}} onClick={() => location.reload()}><span>OSSPITA FOR</span> <img src={ollama}/></figure>
-            <ConversationsSlot activeConversation={activeConversation} setActiveConversation={setActiveConversation}/>
+            <ConversationsSlot conversationStateRef={conversationStateRef} activeConversation={activeConversation} setActiveConversation={setActiveConversation}/>
             <DocumentsSlot/>
-            <PromptsSlot selectedPromptRef={selectedPromptRef} setModalStatus={setModalStatus}/>
+            <PromptsSlot selectedPromptRef={selectedPromptRef} memoizedSetModalStatus={memoizedSetModalStatus} promptsList={promptsList}/>
         </aside>
     )
-}
+}, (prevProps, nextProps) => {
+    return prevProps.activeConversation === nextProps.activeConversation && prevProps.conversationStateRef.current === nextProps.conversationStateRef.current && prevProps.promptsList === nextProps.promptsList;
+})
+
+export default LeftPanel
 
 interface IProps{
     activeConversation : number
     setActiveConversation : (index : number) => void
-    setModalStatus : (visibility : boolean, contentId : string) => void
+    memoizedSetModalStatus : ({visibility, contentId} : {visibility : boolean, contentId : string}) => void
     selectedPromptRef : React.MutableRefObject<string>
+    conversationStateRef: React.MutableRefObject<IConversation>
+    promptsList : IPrompt[]
 }
