911e2f146cc74272b5f112962c0a29a31d131b59
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-10-08 07:43:09 +0200

    *edit* front / IConversation now includes technical datas about the generation

--- src/interfaces/IConversation.ts
+++ src/interfaces/IConversation.ts
@@ -1,11 +1,15 @@
-// conversation = {question, answer : {md, html}, context}
-
 export interface IConversation {
     name : string
     history : IConversationElement[]
     lastModelUsed : string // useful to determine the context format
     // should add displayed : yes/no so that intermediate COT steps could be hidden
     // should have last agent used
+    technicalDatas ? : {
+        promptEvalDuration : number
+        inferenceDuration : number
+        modelLoadingDuration : number
+        wholeProcessDuration  : number
+    }
 }
 
 export interface IConversationElement{
