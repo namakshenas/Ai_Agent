2611447d57734b9fc8ac7e2abc08a23c33de0d4e
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-11-03 10:46:18 +0100

    *add* tests / mockRAGDocumentsList added

---
+++ Front/src/__mocks__/mockRAGDocumentsList.tsx
@@ -0,0 +1,24 @@
+import { IRAGDocument } from "../interfaces/IRAGDocument";
+
+const mockRAGDocumentsList : IRAGDocument[] = [
+    {
+        id : 1,
+        filename : "dracula",
+        size : 100000,
+        selected : false,
+    },
+    {
+        id : 2,
+        filename : "montecristo",
+        size : 200000,
+        selected : false,
+    },
+    {
+        id : 3,
+        filename : "sota",
+        size : 300000,
+        selected : false,
+    },
+]
+
+export default mockRAGDocumentsList
