ff0b88180a267ae74adf571c1e0fea4254b18845
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-11-03 10:44:19 +0100

    *edit* front / updated custom router

--- Front/src/router/CustomRouter.tsx
+++ Front/src/router/CustomRouter.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "../pages/Chat";
-import FirstStart from "../pages/FirstStart";
+import Installation from "../pages/Installation";
 
 function CustomRouter() {
 
     return (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<FirstStart />} />
+            <Route path="/" element={<Installation />} />
             <Route path="/chat" element={<Chat/>} />
-            <Route path="*" element={<FirstStart />} />
+            <Route path="*" element={<Installation />} />
           </Routes>
         </BrowserRouter>
       );
