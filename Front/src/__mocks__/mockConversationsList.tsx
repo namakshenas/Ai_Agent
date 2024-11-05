25b9c4b9bc343c79c15404f3af7ddd84b20a9bf8
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-11-05 12:44:32 +0100

    *add* tests / mockconversations + mockprompts added

---
+++ Front/src/__mocks__/mockConversationsList.tsx
@@ -0,0 +1,25 @@
+import { IConversation } from "../interfaces/IConversation";
+
+const mockConversationsList : IConversation[] =
+    [
+        {
+            name : "2nd Conversation",
+            history : [],
+            lastAgentUsed : "mockAgent",
+            lastModelUsed : "mockModel",
+        },
+        {
+            name : "3rd Conversation",
+            history : [],
+            lastAgentUsed : "mockAgent",
+            lastModelUsed : "mockModel",
+        },
+        {
+            name : "4th Conversation",
+            history : [],
+            lastAgentUsed : "mockAgent",
+            lastModelUsed : "mockModel",
+        },
+    ]
+
+export default mockConversationsList
--- Front/src/__mocks__/mockPromptsList.tsx
+++ Front/src/__mocks__/mockPromptsList.tsx
@@ -58,5 +58,24 @@ const mockPromptsList : IPromptResponse[] = [
         meta : {revision : 0, created : 0, version : 0},
         $loki : 3,
     },
+    {
+      id: 'p0000000004',
+      name: "MockPrompt",
+      prompts: [
+         {
+            version: 1,
+            text: "You are an AI assistant tasked with creating a step-by-step mental reflection plan on how to fulfill a request.",
+            createdAt: new Date().toISOString(),
+         },
+         {
+            version: 2,
+            text: "You are an AI assistant tasked with creating a step-by-step mental reflection plan on how to fulfill a request.",
+            createdAt: new Date().toISOString(),
+         }
+      ],
+      currentVersion: 2,
+      meta : {revision : 0, created : 0, version : 0},
+      $loki : 4,
+  },
 ]
 export default mockPromptsList
