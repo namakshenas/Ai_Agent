63c9ab296a146737e232b9431b7301159af5db3c
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-11-06 11:52:28 +0100

    *add* back / base docs constant

---
+++ Back/constants/fakedocs.js
@@ -0,0 +1,94 @@
+const fakedocs = [
+  {
+    id: 'd0000000001',
+    filename: 'tiny-shakespeare.txt',
+    size: 30089871,
+    selected: false
+  },
+  {
+    id: 'd0000000002',
+    filename: 'user_data.csv',
+    size: 20341178,
+    selected: false
+  },
+  {
+    id: 'd0000000003',
+    filename: 'meeting_minutes.docx',
+    size: 5872,
+    selected: false
+  },
+  {
+    id: 'd0000000004',
+    filename: 'product_catalog.json',
+    size: 23378608,
+    selected: false
+  },
+  {
+    id: 'd0000000005',
+    filename: 'state_of_the_union.txt',
+    size: 8180088,
+    selected: false
+  },
+  {
+    id: 'd0000000006',
+    filename: 'sales_data.xlsx',
+    size: 19818086,
+    selected: false
+  },
+  {
+    id: 'd0000000007',
+    filename: 'article.md',
+    size: 4404,
+    selected: false
+  },
+  {
+    id: 'd0000000008',
+    filename: 'invoice.pdf',
+    size: 3670,
+    selected: false
+  },
+  {
+    id: 'd0000000009',
+    filename: 'employee_records.csv',
+    size: 27368709,
+    selected: false
+  },
+  {
+    id: 'd0000000010',
+    filename: 'project_plan.pptx',
+    size: 13317472,
+    selected: false
+  },
+  {
+    id: 'd0000000011',
+    filename: 'blog_post.txt',
+    size: 870,
+    selected: false
+  },
+  {
+    id: 'd0000000012',
+    filename: 'inventory_list.json',
+    size: 26110361,
+    selected: false
+  },
+  {
+    id: 'd0000000013',
+    filename: 'presentation_notes.doc',
+    size: 6815,
+    selected: false
+  },
+  {
+    id: 'd0000000014',
+    filename: 'user_feedback.xlsx',
+    size: 186,
+    selected: false
+  },
+  {
+    id: 'd0000000015',
+    filename: 'report.pdf',
+    size: 31247908,
+    selected: false
+  }
+]
+
+module.exports = fakedocs
