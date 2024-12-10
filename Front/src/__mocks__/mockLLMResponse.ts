6a3f13b1b652628aae90443df1066483115999f9
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-12-10 19:56:59 +0100

    *edit* test / mocks llm responses

---
+++ Front/src/__mocks__/mockLLMResponse.ts
@@ -0,0 +1,15 @@
+const mockLLMResponse = {
+    model: "string",
+    created_at: "string",
+    response: "here is my mock response",
+    done: true,
+    context: [],
+    total_duration: 100,
+    load_duration: 10,
+    prompt_eval_count: 100,
+    prompt_eval_duration: 10,
+    eval_count: 200,
+    eval_duration: 50,
+}
+
+export default mockLLMResponse
